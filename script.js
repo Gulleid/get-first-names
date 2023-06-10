aaa72437eb7223fdb1a0db17d1a5f916e8420124
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-10 19:33:47 +0300

    first names

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Good Luck 💪🏾
+// Good Luck 💪🏾
+
+Getfirstnames =[["ahmed ","aisha ","Aamina"],["jama","khadija","layla"],["mohamed","Ali"]]
+console.log(Getfirstnames[0][0])
+console.log(Getfirstnames[1][0])
+console.log(Getfirstnames[2][0])
